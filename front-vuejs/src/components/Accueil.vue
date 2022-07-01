bfea7ab03d8bedc848f56e66e29c65b579f6c36b
Sandra <sandra.villot038@gmail.com>
2022-07-01 11:11:17 +0200

    creation de la vue pour les restos et employes

--- front-vuejs/src/components/Accueil.vue
+++ front-vuejs/src/components/Accueil.vue
@@ -1,30 +1,59 @@
 <template>
-    <div>
-        <h1>Liste de mes restaurants</h1>
-        <!-- <p @click="increment">Il y a {{total}} restaurants</p> -->
-        <p :key="index" v-for="(restaurant, index) in restaurants"> {{restaurant}}</p>
-    </div>
+  <div>
+    <h1>Liste de mes restaurants</h1>
+    <!-- <p @click="increment">Il y a {{total}} restaurants</p> -->
+
+    <p :key="index" v-for="(restaurant, index) in restaurants">
+      {{ restaurant.name }} à {{ restaurant.city }} <br>
+      {{ restaurant.nbcouverts }} couverts <br> Terrasse : {{ restaurant.terrasse }} <br>
+      Parking : {{ restaurant.parking }} <br><button  v-on:click="suprim(restaurant.id_restaurants)"  >Supprimer le restaurant</button><br> <br>
+      <strong>Employes : </strong>
+      <span :key="index" v-for="(employe, index) in employes">
+        <span v-if="restaurant.id_restaurants === employe.restaurant_id">
+          {{ employe.first_name }} {{ employe.last_name }} <button  v-on:click="suprimEmploye(employe.id_employes)"  >X</button>
+        </span>
+      </span>
+    </p>
+  </div>
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 export default {
-    name: 'Accueil',
-    data() {
-        return {
-            restaurants: null
-            //total: 3
-        }
-    },
-    mounted() {
-        
-    }
-    // methods: {
-    //     increment() {
-    //         this.total += 1;
-    //     }
-    // }
-}
+  name: "Accueil",
+  data() {
+    return {
+      restaurants: null,
+      employes: null,
+      //total: 3
+    };
+  },
+  mounted() {
+    axios
+      .get("http://127.0.0.1:5000/restaurants")
+      .then((res) => (this.restaurants = res.data))
+      .catch(
+        (error) => (this.restaurants = [{ name: "Erreur de chargement" }])
+      );
+    axios
+      .get("http://127.0.0.1:5000/employes")
+      .then((res) => (this.employes = res.data))
+      .catch((error) => (this.employes = [{ name: "Erreur de chargement" }]));
+  },
+  methods: {
+      suprim(index) {
+        // console.log(index);
+        // axios.delete("http://127.0.0.1:5000/restaurants/" + index )
+      },
+      suprimEmploye(index) {
+          // console.log(index);
+          // axios.delete("http://127.0.0.1:5000/employe/" + index)
+      }
+  //     increment() {
+  //         this.total += 1;
+  //     }
+  }
+};
 </script>
 
 <style scoped>
