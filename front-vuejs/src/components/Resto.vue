9f47058a1f37233e1940c39c902e18e40f11beb1
Sandra <sandra.villot038@gmail.com>
2022-07-04 12:44:17 +0200

    formulaire d'insertion resto ok

--- front-vuejs/src/components/Resto.vue
+++ front-vuejs/src/components/Resto.vue
@@ -2,73 +2,88 @@
     <div class="home">
 
         <h1>Ajouter un restaurant</h1>
-        <form id="app" @submit="checkForm" method="post">
         <p>
             <label for="name">Nom</label>
-            <input id="name" v-model="name" type="text" name="name">
+            <input id="name" v-model="form.name" type="text" name="name">
         </p>
         <p>
             <label for="city">Ville</label>
-            <input id="city" v-model="city" type="text" name="city">
+            <input id="city" v-model="form.city" type="text" name="city">
         </p>
         <p>
             <label for="nbcouverts">Nombre de couverts</label>
-            <input id="nbcouverts" v-model="nbcouverts" type="text" name="nbcouverts">
+            <input id="nbcouverts" v-model="form.nbcouverts" type="text" name="nbcouverts">
         </p>
         <p> Terrasse :
             <label for="oui">Oui</label>
-            <input id="oui" type="radio" name="terrasse" value="oui">
+            <input id="oui" type="radio"  v-model="form.terrasse" value="oui">
 
             <label for="non">Non</label>
-            <input id="non" type="radio" name="terrasse" value="non">
+            <input id="non" type="radio" v-model="form.terrasse" value="non">
         </p>
         <p> Parking :
             <label for="oui">Oui</label>
-            <input id="oui" type="radio" name="parking" value="oui">
+            <input id="oui" type="radio" v-model="form.parking" value="oui">
 
             <label for="non">Non</label>
-            <input id="non" type="radio" name="parking" value="non">
+            <input id="non" type="radio" v-model="form.parking" value="non">
         </p>
         <p>
-            <input type="submit" value="Valider">
+            <button v-on:click="submit">Valider</button>
         </p>
-        </form>
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
     name: 'Resto',
-    return: {
-    el: '#app',
-    data: {
-        errors: [],
-        name: null,
-        city: null,
-        nbcouverts: null,
-        terrasse: null,
-        parking: null
-    },
-    },
-    methods: {
-        checkForm: function (e) {
-            this.errors = [];
-
-            if (!this.name) {
-                this.errors.push("Name required");
-            }
-            if (!this.city) {
-                this.errors.push("City required");
-            }
-            if (!this.nbcouverts) {
-                this.errors.push("Name required");
-            }
-            if (!this.terrasse) {
-                this.errors.push("Terrasse required");
-            }
-            if (!this.parking) {
-                this.errors.push("Name required");
+    data() {
+        return {
+            form: {
+                name: null,
+                city: null,
+                nbcouverts: null,
+                terrasse: null,
+                parking: null
             }
         }
+    },
+    methods: {
+        submit() {
+            axios.post('http://127.0.0.1:5000/restaurant', this.form)
+        }
     }
+    // return: {
+    // el: '#app',
+    // data: {
+    //     errors: [],
+    //     name: null,
+    //     city: null,
+    //     nbcouverts: null,
+    //     terrasse: null,
+    //     parking: null
+    // },
+    // },
+    // methods: {
+    //     checkForm: function (e) {
+    //         this.errors = [];
+
+    //         if (!this.name) {
+    //             this.errors.push("Name required");
+    //         }
+    //         if (!this.city) {
+    //             this.errors.push("City required");
+    //         }
+    //         if (!this.nbcouverts) {
+    //             this.errors.push("Name required");
+    //         }
+    //         if (!this.terrasse) {
+    //             this.errors.push("Terrasse required");
+    //         }
+    //         if (!this.parking) {
+    //             this.errors.push("Name required");
+    //         }
+    //     }
+    // }
 }
 </script>
